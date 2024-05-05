50957bf238ccf1b6d679f3f02c62d6088830a663
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2024-05-05 01:55:56 -0300

    trabalhando na parte de criação de componente e ja fazendo a parte de funcionalidades com js...

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,22 +1,32 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import { useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import { useState } from "react"
+
+
 
 function Calculator() {
+  const [currentValue, setCurrentValue] = useState('0')
+  const [podingOperation, setPedingOperatoion] = useState(null)
+  const [pedingValue, setPedingValue] = useState(null)
+  const [compliteOperation, setCompleteOperation] = useState('')
 
-  const keypadNumber = ["1","2","3","4","5","6","7","8","9"]
-  const operations = ["+","-","*","/"]
+ 
+  const keypadNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  const operations = ["+", "-", "*", "/"]
   return (
-    <div className='flex justify-center items-center h-screen w-full'>
-      <div className='complete-operation'>3 + 4 + = </div>
-      <div className='display'> 6</div>
-      <div className='buttons'>
+    <div className='flex flex-col items-center justify-center w-[200px] m-auto h-screen'>
+      <div className='w-full h-[50px] border mb-3 text-right p-1'>3 + 4  = </div>
+      <div className='w-full h-[50px] border mb-3 text-right p-1 overflow-auto'> 7</div>
+      <div className='grid  grid-cols-4 gap-2'>
+        <button className="grid p-4 font-bold bg-slate-400">AC</button>
         {keypadNumber.map((num) => (
-          <button key={num}>{num}</button>
+          <button className="p-4 font-bold bg-slate-400" key={num}>{num}</button>
         ))}
         {operations.map((operation) => (
-          <button key={operation}>{operation}</button>
+          <button className="p-4 font-bold bg-slate-400" key={operation}>{operation}</button>
         ))}
+        <button className="grid p-4 font-bold bg-slate-400">=</button>
+
       </div>
     </div>
   )
